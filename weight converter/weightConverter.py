a052909ce0914dded2ec5e4e6c9e852927712cdc
Spenser Tan <falconeagle21@gmail.com>
2021-08-29 00:56:32 +1000

    bug fixes

--- weight converter/weightConverter.py
+++ weight converter/weightConverter.py
@@ -1,19 +1,31 @@
 from tkinter import *
 
-
 unitTypes = {
     'Tonnes': pow(10, -3),
     'Kilograms': pow(10, 0),
     'Grams': pow(10, 3),
     'Milligrams': pow(10, 6),
     'Micrograms': pow(10, 9),
+    'Pounds': 2.20462,
+    'Ounces': 35.274,
+    'Imperial Ton': 0.000984207,
+    'US Ton': 0.00110231,
+    'Stone': 0.157473,
 }
 
 
 # converts weight from one type of weight to another
 def calculate():
-    convertedValue = weight.get() * (unitTypes[convertedWeightType.get()] / unitTypes[weightType.get()])
-    convertedWeight.set(convertedValue)
+    try:
+        convertedValue = weight.get() * (unitTypes[convertedWeightType.get()] / unitTypes[weightType.get()])
+        convertedWeight.set(convertedValue)
+    except TclError:
+        newWindow = Tk()
+        newWindow.geometry("155x40")
+        newWindow.title('Error')
+        newWindow.config(bg='white')
+        newWindow.resizable(False, False)
+        Label(newWindow, bg='white', text='Please enter a number.', font=('helvetica',10,'bold')).grid(row=1,column=1)
 
 
 # initialise the window
@@ -21,6 +33,7 @@ wc = Tk()
 wc.geometry('250x100')
 wc.config(background='white')
 wc.title('Weight converter')
+wc.resizable(False, False)
 
 # create an option menu for the weight to be converted
 weightType = StringVar(wc)
@@ -31,7 +44,7 @@ menuWeight.grid(row=1, column=1)
 # entry to insert a number for the weight to be converted
 weight = IntVar(wc)
 weight.set(0)
-weightInput = Entry(wc, textvariable=weight, width=15, font=('times', 15))
+weightInput = Entry(wc, text='Input',textvariable=weight, width=15, font=('times', 15))
 weightInput.grid(row=1, column=2)
 
 # create an option menu for the weight to be converted
@@ -42,7 +55,7 @@ menuWeight.grid(row=2, column=1)
 
 # entry for displaying the converted number
 convertedWeight = StringVar(wc)
-convertedWeight.set(0)
+convertedWeight.set("Output")
 convertedWeightInput = Entry(wc, textvariable=convertedWeight, width=15, font=('times', 15))
 convertedWeightInput.grid(row=2, column=2)
 
